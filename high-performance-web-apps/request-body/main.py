57f21d398b4c994600038bdc02d5ee2af44be673
Mehmet Tarhan <hi@memtarhan.com>
2025-09-03 06:54:48 +0100

    <High Performance Web Apps> : Pydantic Model As Parameter

---
+++ high-performance-web-apps/request-body/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+
+
+class Product(BaseModel):
+    product_id: int
+    name: str
+    price: float
+    stock: int
+
+
+app = FastAPI()
+
+products = []
+
+
+@app.post("/product/")
+async def add_new_product1(product: Product):
+    products.append(product)
+    return products
+
+
+@app.post("/product/")
+async def add_new_product2(product: Product):
+    data = product.model_dump()
+    price = data['price']
+    if price > 5000:
+        data['price'] = price + price * 0.1
+        product.price = data['price']
+
+    products.append(product)
+    return products
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
