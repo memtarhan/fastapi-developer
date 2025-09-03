8f3afe4e7d2f96543f1190324f748f27c063ab9b
Mehmet Tarhan <hi@memtarhan.com>
2025-09-03 06:41:54 +0100

    <High Performance Web Apps> : Path and Query validating

--- high-performance-web-apps/hello-world/main.py
+++ high-performance-web-apps/hello-world/main.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
 
 app = FastAPI()
 
@@ -11,33 +11,80 @@ async def index():
     return {"message": "Hello World"}
 
 
-@app.get("/{name}/{id}")
-async def user(name: str, id: int):
-    return {"name": name, "id": id}
+# @app.get("/{name}/{id}")
+# async def user(name: str, id: int):
+#     return {"name": name, "id": id}
+#
+#
+# # Path Parameters
+# @app.get("/employee/{name}/{age}")
+# async def get_employee1(name: str, age: int):
+#     return {"name": name, "age": age}
+#
+#
+# # Query Parameters
+# @app.get("/employee/{name}")
+# async def get_employee2(name: str, age: int):
+#     return {"name": name, "age": age}
+#
+#
+# # Default Parameters
+# @app.get("/employee/{name}")
+# async def get_employee3(name: str, age: int = 20):
+#     return {"name": name, "age": age}
+#
+#
+# # Optional Parameters
+# @app.get("/employee/{name}")
+# async def get_employee4(name: str, age: Optional[int] = None):
+#     return {"name": name, "age": age}
+#
+#
+# Order of Parameters
+@app.get("/employee/{name}/branch/{branch_id}")
+async def get_employee5(name: str, brname: str, branch_id: int, age: Optional[int] = None):
+    employee = {'name': name, 'Branch': brname, 'Branch ID': branch_id, 'age': age}
+    return employee
 
 
-# Path Parameters
-@app.get("/employee/{name}/{age}")
-async def get_employee1(name: str, age: int):
-    return {"name": name, "age": age}
+# Validating String Parameter
+@app.get("/employee/{name}/branch/{branch_id}")
+async def get_employee6(branch_id: int,
+                        name: str = Path(..., min_length=10),
+                        brname: str = Query(None, min_length=5, max_length=10),
+                        age: Optional[int] = None):
+    employee = {
+        'name': name,
+        'branch': brname,
+        'branch_id': branch_id,
+        'age': age,
+    }
+
+    return employee
 
 
-# Query Parameters
-@app.get("/employee/{name}")
-async def get_employee2(name: str, age: int):
-    return {"name": name, "age": age}
+# Validating Numeric Parameters
+"""
+The following types of validation criteria can be specified in the Path()
+or Query() constructor:
+• gt: Greater than
+• ge: Greater than or equal
+• lt: Less than
+• le: Less than or equal
+"""
 
 
-# Default Parameters
-@app.get("/employee/{name}")
-async def get_employee3(name: str, age: int = 20):
-    return {"name": name, "age": age}
+@app.get("/employee/{name}/branch/{branch_id}")
+async def get_employee7(name: str, brname: str, branch_id: int = Path(..., gt=0, le=100),
+                        age: int = Query(None, ge=20, lt=61)):
+    employee = {
+        'name': name,
+        'branch': brname,
+        'branch_id ID': branch_id,
+        'age': age
+    }
 
-
-# Optional Parameters
-@app.get("/employee/{name}")
-async def get_employee4(name: str, age: Optional[int] = None):
-    return {"name": name, "age": age}
+    return employee
 
 
 if __name__ == "__main__":
